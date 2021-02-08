9533180fc1b6e3e598b7ab1fae309e188eb653fc
Mumtaz <mumtaz.maqsood@yahoo.com>
2021-02-08 19:43:03 +0100

    test added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/quiz app/i);
+  const linkElement = screen.getByText(/Loading/i);
   expect(linkElement).toBeInTheDocument();
 });
